721e9e8d1b0c343a40fea35de3cbb3083feaf632
GIOVANI CHADAVI HIDAYAT <73802990+giovanichadavi@users.noreply.github.com>
2021-10-28 21:40:39 +0800

    Update posttest.cpp

--- posttest.cpp
+++ posttest.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
 using namespace std;
 
 struct crypto{
@@ -9,10 +13,9 @@ struct crypto{
 	long harga_tertinggi;
 	long harga_terendah;
 };
-crypto batas [100];
-int a,b,c,d;
-int memilih;
 
+int a,b,c,d;
+int jumlah = 0;
 crypto coin [10];
 crypto *pointer = coin;
 
@@ -52,36 +55,37 @@ void tambahdata(){
 
 	cout << " Masukkan Jumlah data yang ingin ditambahkan = ";
 	cin >> b ;
-
+	d=0;
 	
-	for (int i = 0; i < b ;i++){
-
-		cout << "\nData Ke : " << i + 1 << endl;
-		cout << "Nama Crypto = ";
-		cin >> pointer[i].nama_crypto;
+	for ( c = 0; c < b ; c++){
 		
-		cout << "Harga Crypto = ";
-		cin >> pointer[i].harga_crypto;
+
+		cout << "\nData Ke : " << c + 1 << endl;
+		cout << "Nama Crypto = ";
+		cin >> pointer[a].nama_crypto;
+		
+		cout << "Harga Crypto = Rp.";
+		cin >> pointer[a].harga_crypto;
 		
 		cout << "Suplai = ";
-		cin >> pointer[i].suplai;
+		cin >> pointer[a].suplai;
 		
-		cout << "Harga Terendah = ";
-		cin >> pointer[i].harga_terendah;
+		cout << "Harga Terendah = Rp.";
+		cin >> pointer[a].harga_terendah;
 		
-		cout << "Harga Tertinggi = ";
-		cin >> pointer[i].harga_tertinggi;
+		cout << "Harga Tertinggi = Rp.";
+		cin >> pointer[a].harga_tertinggi;
 		
-
+		a++;
 	
 		
 		
 		data :
-		cout <<"\nNama Crypto : " << pointer[i].nama_crypto<< endl;
-		cout <<"Harga Crypto : " << pointer[i].harga_crypto<< endl;
-		cout <<"Suplai : " << pointer[i].suplai;
-		cout <<"Harga Terendah : " << pointer[i].harga_terendah;
-		cout <<"Harga Tertinggi : " << pointer[i].harga_tertinggi << endl;
+		cout <<"\nNama Crypto : " << pointer[a].nama_crypto<< endl;
+		cout <<"Harga Crypto : " << pointer[a].harga_crypto<< endl;
+		cout <<"Suplai : " << pointer[a].suplai;
+		cout <<"Harga Terendah : " << pointer[a].harga_terendah;
+		cout <<"Harga Tertinggi : " << pointer[a].harga_tertinggi << endl;
 		cout << endl;
 		system ("cls");
 		cout << "Data Berhasil Disimpan !!!"<< endl;
@@ -92,27 +96,26 @@ void tambahdata(){
 
 void readdata(){
 
-
-	cout << "==================================================================\n";
+	int i,j;
+	cout << "------------------------------------------------------------------\n";
 	cout << "============== DATA CRYPTOCURRENCY YANG TERSIMPAN ================\n";
-	cout << "==================================================================\n";
+	cout << "------------------------------------------------------------------\n\n";
 	
 
+	j=0;
+	for ( i = 0; i < a ;i++){
+		j=j+1;
 
-	for (int i = 0; i < b ;i++){
-		
 
-		
 		cout << "\nData Ke : " <<i + 1<< endl;
 		cout <<"\nNama Crypto : " << pointer[i].nama_crypto<< endl;
-		cout <<"Harga Crypto : " << pointer[i].harga_crypto<< endl;
+		cout <<"Harga Crypto :Rp. " << pointer[i].harga_crypto<< endl;
 		cout <<"Suplai : " << pointer[i].suplai << endl;
-		cout <<"Harga Terendah : " << pointer[i].harga_terendah << endl;
-		cout <<"Harga Tertinggi : " << pointer[i].harga_tertinggi << endl;
+		cout <<"Harga Terendah :Rp. " << pointer[i].harga_terendah << endl;
+		cout <<"Harga Tertinggi :Rp. " << pointer[i].harga_tertinggi << endl;
+		cout << "==========================================================" << endl;
 		cout << endl;
 		
-		
-	
 	}
 	
 }
@@ -124,16 +127,86 @@ void hapusdata(){
 	cout << "\nSilahkan Pilih Nomor Data Yang Ingin Di Hapus : ";
 	cin >> x;
 	
-	cout <<"Data Nomor --> "<< x << "Terhapus" << endl;
-	
+	cout <<"Data Nomor --> "<< x << " Terhapus " << endl;
 	y=x-1;
 	a--;
-	for(int i = y ; i < y ; i++){
-		coin[i]=coin[i+1];
-		y=x-1;
+	for(int i = y ; i < a ; i++){
+		pointer[i]=pointer[i+1];
+		getch();
 	}
+	system("cls");
 }
 	
+	
+void swap(crypto *a, crypto *b){
+	crypto t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition (crypto arr[], int low, int high, int pilih_menu) {
+	crypto pivot = arr[high];
+	int m;
+	int i = (low - 1);
+
+
+	for (int m = low; m <= high - 1; m++) {
+		if (pilih_menu == 1) {
+			if (arr[m].nama_crypto < pivot.nama_crypto)	{	
+				i++;
+				swap(&arr[i], &arr[m]);
+				cout << arr;
+				
+			}
+		} else if (pilih_menu == 2) {
+			if (arr[m].nama_crypto > pivot.nama_crypto)	{	
+				i++;
+				swap(&arr[i], &arr[m]);
+			}
+		} else if (pilih_menu == 3) {
+			if (arr[m].suplai < pivot.suplai)	{	
+				i++;
+				swap(&arr[i], &arr[m]);
+			}
+		} else if (pilih_menu == 4) {
+			if (arr[m].suplai > pivot.suplai)	{	
+				i++;
+				swap(&arr[i], &arr[m]);
+			}
+		}	
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+	
+void quicksort(crypto arr[], int low, int high, int pilih_menu) {
+	if (low < high) {
+		int pi = partition(arr, low  , high, pilih_menu);
+		quicksort(arr, low, pi - 1, pilih_menu);
+		quicksort(arr, pi + 1, high, pilih_menu);
+	}
+	readdata();
+}
+
+void urut_data(crypto *coin, int &jumlah){
+	int pilih_menu;
+
+	
+	cout << "Silahkan Pilih Jenis Pengurutan"<< endl;
+	cout << " 1. Mengurutkan Data Sesuai Nama Secara Ascending" << endl;
+	cout << " 2. Mengurutkan Data Sesuai Nama Secara Descending" << endl;
+	cout << " 3. Mengurutkan Data Sesuai Jumlah Suplai Secara Ascending" << endl;
+	cout << " 4. Mengurutkan Data Sesuai Jumlah Suplai Secara Descending" << endl << endl;
+	cout << " Silahkan Pilih Menu Pengurutan : " ;
+	cin >> pilih_menu;
+
+	quicksort(&coin[0],0, jumlah, pilih_menu);
+	jumlah+=1;
+
+}
+
+
+
 
 void updatedata(){
 	
@@ -175,7 +248,6 @@ void keluar(){
 }
 
 
-
 int main(){
 	int pilih;
 	
@@ -189,7 +261,8 @@ int main(){
 	cout << " 2. Menampilkan Data Coin Crypto " << endl;
 	cout << " 3. Mengupdate Data Crypto " << endl;
 	cout << " 4. Menghapus Data Crypto " << endl;
-	cout << " 5. Keluar " << endl;
+	cout << " 5. Mengurutkan Data" << endl;
+	cout << " 6. Keluar " << endl;
 	cout << " ================================\n"<<endl;
 	
 	cout << " Masukkan Nomor Menu Yang Anda Inginkan : ";
@@ -217,6 +290,15 @@ int main(){
 		
 	}
 	if(pilih==5){
+		
+		system("cls");
+		urut_data(&coin[0],jumlah);
+		
+		goto menu;
+
+	
+	}
+	if(pilih==6){
 		keluar();
 	}
 }
